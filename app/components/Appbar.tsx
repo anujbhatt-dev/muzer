3121ff09aeae4466f6cf59ffb2051f7b13d7c679
anujbhatt023@gmail.com <anujbhatt023@gmail.com>
2025-07-19 17:23:14 +0400

    create schema

---
+++ app/components/Appbar.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { SignedIn, SignedOut, SignOutButton, useClerk, UserButton, UserProfile, useUser } from '@clerk/clerk-react'
+import { SignInButton } from '@clerk/nextjs'
+import { LogOut } from 'lucide-react'
+import React from 'react'
+
+
+export default function Appbar() {
+    const {signOut} = useClerk()
+    
+    
+  return (
+    <div className='flex w-full justify-between p-4 px-20 bg-zinc-900 text-white font-semibold border-b border-black/40 shadow-md items-center'>
+        <div className='font-extrabold tracking-widest text-xl'>
+            MUZER
+        </div>
+        <div>
+            <div className='flex gap-5' >
+                    <SignedIn>                        
+                        <UserButton/>
+                        <LogOut onClick={()=>signOut()}/>
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton mode="modal"/>
+                    </SignedOut>
+            </div>
+        </div>
+    </div>
+  )
+}
