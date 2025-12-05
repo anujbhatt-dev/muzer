7b189ad6618b0f4712d787216ca640300a551c70
anujbhatt-dev <anujbhatt023@gmail.com>
2025-12-06 03:29:39 +0400

    fix: ignore eslint removed

--- next.config.ts
+++ next.config.ts
@@ -17,10 +17,7 @@ const nextConfig: NextConfig = {
         hostname:"fastly.picsum.photos"
       }
     ],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
